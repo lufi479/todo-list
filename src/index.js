05edf5efcb93ef5f746aa5a8e21a50d567a38475
lufi479 <135998525+lufi479@users.noreply.github.com>
2023-07-20 12:18:02 -0700

    Add Functionality for editing a project name or to delete it

--- src/index.js
+++ src/index.js
@@ -52,7 +52,8 @@ document.querySelector(".project-section").appendChild(projForm);
 
 createProjectBtn.addEventListener("click", function(e) {
     projForm.classList.remove("hidden");
-    projForm.classList.add("shown");
+    //projForm.classList.add("shown");
+    createProjectBtn.classList.add("hidden")
 })
 
 function display () {
@@ -79,9 +80,15 @@ function display () {
     deleteDiv.classList.add("delete-project");
     projBtnsContainterDiv.appendChild(deleteDiv);
 
+    projBtnsContainterDiv.classList.add("hidden");
+
     card.appendChild(projBtnsContainterDiv);
 
-    card.addEventListener("click", selectProject);
+    card.addEventListener("click", carryOutProjectEvent);
+
+    card.dataset.id = i;
+
+    projectList[i].id = i;
 
     projListDiv.appendChild(card);
   }
@@ -105,15 +112,116 @@ function closeForm() {
   projForm.classList.remove("shown");
   projForm.classList.add("hidden");
   projForm.reset();
+  createProjectBtn.classList.remove("hidden")
 }
 
 display();
 
+function carryOutProjectEvent(e){
+  if (e.target.closest(".edit-project")){
+    console.log("edit");
+    editProject(e);
+  }
+  else if (e.target.closest(".delete-project")){
+    deleteProject(e);
+  }
+  else if (e.target.closest(".project") || e.target.closest(".project-name")){
+    console.log("lol");
+    selectProject(e);
+  }
+  
+}
+
+function deleteProject(e){
+  let selectedProject = e.currentTarget;
+  let id = selectedProject.dataset.id;
+  projectList.splice(id, 1);
+  display();
+}
+
 function selectProject(e){
+  e.stopPropagation();
   let selected = document.querySelector(".selected-project");
   if (selected){
     selected.classList.remove("selected-project");
+    selected.querySelector(".project-buttons-container").classList.add("hidden");
   }
   console.log(e);
   e.target.classList.add("selected-project");
+  e.target.querySelector(".project-buttons-container").classList.remove("hidden");
 }
+
+function editProject(e){
+  let selectedProject = e.currentTarget;
+  let id = selectedProject.dataset.id;
+  //selectedProject.classList.add("hidden");
+
+  selectedProject.removeEventListener("click", carryOutProjectEvent);
+
+  let changeForm = createChangeForm(id);
+
+  let children = selectedProject.children;
+
+  for(let i = 0; i < children.length; i++){
+    children[i].classList.add("hidden");
+  }
+
+  selectedProject.appendChild(changeForm);
+
+  addChangeFormFunctionality(selectedProject, changeForm, id);
+
+  
+}
+
+function createChangeForm(id){
+  const changeForm = document.createElement("form");
+  projForm.action = "#";
+  projForm.classList.add("change-project-form");
+
+  const enterNewName = document.createElement("input");
+  enterNewName.type = "text";
+  enterNewName.id = "proj-name";
+  enterNewName.name = "proj-name";
+  enterNewName.value = projectList[id].name;
+
+  const changeProj = document.createElement("button");
+  changeProj.type = "submit";
+  changeProj.id = "submit-change-name";
+  changeProj.textContent = "change";
+
+  const cancelChangeProj = document.createElement("button");
+  cancelChangeProj.id = "cancel-change-name";
+  cancelChangeProj.textContent = "cancel";
+  cancelChangeProj.type = "reset";
+
+
+  changeForm.appendChild(enterNewName);
+  changeForm.appendChild(changeProj);
+  changeForm.appendChild(cancelChangeProj);
+  return changeForm;
+}
+
+function addChangeFormFunctionality (selectedProject, changeForm, id){
+  changeForm.addEventListener("submit", function(e) {
+    e.preventDefault();
+    let formData = new FormData(this);
+    let nameInput = formData.get("proj-name");
+    projectList[id].name = nameInput;
+    closeChangeForm();
+    selectedProject.querySelector(".project-name").textContent = projectList[id].name;
+  });
+
+  changeForm.addEventListener("reset", function (e){
+    closeChangeForm();
+  });
+
+  function closeChangeForm(){
+    selectedProject.removeChild(changeForm);
+    let children = selectedProject.children;
+    for(let i = 0; i < children.length; i++){
+      children[i].classList.remove("hidden");
+    }
+    selectedProject.addEventListener("click", carryOutProjectEvent);
+  }
+}
+
